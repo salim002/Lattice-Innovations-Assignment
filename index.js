1203f4890885a433115faf633a8c5e5b9c80ad57
MD SALIM <mdsalim.iiitr@gmail.com>
2024-02-08 18:04:09 +0530

    Done initial setup for express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
